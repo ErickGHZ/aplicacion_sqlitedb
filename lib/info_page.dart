0627d1b0c807aeebef4fea59a45fef1845738321
Erick <1722110095@gmail.com>
2023-10-31 20:28:11 -0600

    Acomodo de la infromacion de infopage

--- lib/info_page.dart
+++ lib/info_page.dart
@@ -14,36 +14,35 @@ class InfoPage extends StatelessWidget {
       body: Column(
           crossAxisAlignment: CrossAxisAlignment.start ,
           children: [
-            // Muestra la imagen usando Image.memory si photoName no es nulo
             Container(
               width: 400,
               height: 250,
               child: Utility.ImageFromBase64String(student.photoName!),
             ),
-            SizedBox(height: 40), // Espacio entre la imagen y los Text Widgets
+            SizedBox(height: 40),
             Text(
               'Nombre: ${student.name}',
-              style: TextStyle(fontSize: 25), // Tamaño de fuente más grande
+              style: TextStyle(fontSize: 25),
             ),
             SizedBox(height: 20),
             Text(
               'Apellido Paterno: ${student.apepa}',
-              style: TextStyle(fontSize: 25), // Tamaño de fuente más grande
+              style: TextStyle(fontSize: 25),
             ),
             SizedBox(height: 20),
             Text(
               'Apellido Materno: ${student.apema}',
-              style: TextStyle(fontSize: 25), // Tamaño de fuente más grande
+              style: TextStyle(fontSize: 25),
             ),
             SizedBox(height: 20),
             Text(
               'Email: ${student.email}',
-              style: TextStyle(fontSize: 25), // Tamaño de fuente más grande
+              style: TextStyle(fontSize: 25),
             ),
             SizedBox(height: 20),
             Text(
               'Teléfono: ${student.tel}',
-              style: TextStyle(fontSize: 25), // Tamaño de fuente más grande
+              style: TextStyle(fontSize: 25),
             ),
           ],
         ),
